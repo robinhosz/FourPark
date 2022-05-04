1a2ff37bdffd5a213641549d72828394c5874e82
Pedro Augusto <pedro4032@hotmail.com>
2022-05-04 13:34:51 -0300

    Update Veiculo
    Criei os Getters, função para cadastrar o veículo e acrescentei o atributo documento

--- src/br/com/fourcamp/fourpark/Veiculo.java
+++ src/br/com/fourcamp/fourpark/Veiculo.java
@@ -1,19 +1,54 @@
 package br.com.fourcamp.fourpark;
 
+import java.util.Scanner;
+
 public class Veiculo {
-	
-	private String proprietario;
+
 	private String modelo;
 	private String placa;
+	private String proprietario;
+	private String documento;
 	
-	public Veiculo(String proprietário, String modelo, String placa) {
-		this.proprietario = proprietário;
-		this.modelo = modelo;
-		this.placa = placa;
+	public Veiculo() {
+		
+	}
+
+	public String getProprietario() {
+		return proprietario;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+	
+	public String getDocumento() {
+		return documento;
+	}
+	
+	public void cadastraVeiculo() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite o modelo do veículo: ");
+		modelo = sc.nextLine();
+		
+		System.out.print("Digite a placa do veículo: ");
+		placa = sc.next();
+		
+		System.out.print("Digite o nome do proprietário: ");
+		proprietario = sc.nextLine();
+		
+		System.out.print("Digite o documento do proprietário: ");
+		documento = sc.next();
 	}
 
 	@Override
 	public String toString() {
-		return "Veiculo: " + modelo + ", com a placa " + placa + ", de " + proprietario;
+		return "Modelo: " + modelo + ", placa: " + placa + ", proprietario: " + proprietario + ", documento:"
+				+ documento;
 	}
+	
 }
